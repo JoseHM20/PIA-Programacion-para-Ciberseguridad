c54be5e9b0cdb71a41e938659f0de8b50c5aff1c
Gerardo0202 <88344050+Gerardo0202@users.noreply.github.com>
2021-10-31 14:29:49 -0600

    Add comments

--- MetaIMG.py
+++ MetaIMG.py
@@ -13,27 +13,27 @@ def printMeta(ruta):
     print("\n")
     time.sleep(2)
 
-    os.mkdir("METADATA")
-    os.chdir("METADATA")
+    os.mkdir("METADATA") #Create a directory with the name "METADATA"
+    os.chdir("METADATA") # Moves to the directory "METADATA"
     sources = []
     list = []
     for subdir, dirs, files in os.walk(ruta):
         for file in files:
-            filepath = subdir + os.sep + file
-            if filepath.endswith(".jpg") or filepath.endswith(".png"):
+            filepath = subdir + os.sep + file #Go to the subdirectory to find the img
+            if filepath.endswith(".jpg") or filepath.endswith(".png"): #Searches if there is a file with .jpg or .png
              try:
-                imagename = filepath
+                imagename = filepath 
              except OSError:
                 print("No se encontró la img")
              else:
                 sources.append(imagename)
                 print(imagename)
-                f = open(f"{file}metadata.txt","w+")
+                f = open(f"{file}metadata.txt","w+") #Open a file with permission to write
                 image = Image.open(imagename)
                 # extract EXIF data
                 exifdata = image.getexif()
                 for tag_id in exifdata:
-                    tag = TAGS.get(tag_id, tag_id)
+                    tag = TAGS.get(tag_id, tag_id) #We get the data of the img 
                     data = exifdata.get(tag_id)
                     # decode bytes
                     if isinstance(data, bytes):
@@ -42,7 +42,7 @@ def printMeta(ruta):
                     x = f"{tag:25}: {data}"
                     f.write(x)
                 f.close()
-    with open("ElementsIMG.txt","w") as f:
+    with open("ElementsIMG.txt","w") as f: #Open a file to write the output 
         for i in sources:
-            f.write(i+"\n")
-    os.chdir("../")
+            f.write(i+"\n") #Write the results
+    os.chdir("../") #Exit the directory
